4dadcb1e690958b11e337dc3588d100195faa9cb
Sebastian Román Sánchez <sebastian.romans@uqvirtual.edu.co>
2024-04-21 21:50:13 -0500

    Update Puesto.java
    Se corrigieron errores de sintaxis en clase Puesto

--- src/main/java/co/edu/uniquindio/poo/Puesto.java
+++ src/main/java/co/edu/uniquindio/poo/Puesto.java
@@ -1,42 +1,42 @@
 
 package co.edu.uniquindio.poo;
-
 /**
  *
  * @author Sebastian Román - Yefry Fajardo - Santiago Gordillo
  */
+
 class Puesto {
     private final int i;
     private final int j;
-    private Vehiculo vehiculo; // Vehiculo que ocupa un puesto, es null si esta vacío
-    
-    public Puesto (int i, int j){
+    private Vehiculo vehiculo; // Vehículo que ocupa un puesto, es null si está vacío
+
+    public Puesto(int i, int j) {
         this.i = i;
-        this.j= j;
-        this.vehiculo = null; // Puesto inicia sin vehiculo
-        
+        this.j = j;
+        this.vehiculo = null; // Puesto inicia sin vehículo
     }
-    public boolean estaOcupado(){ // Indica si el puesto esta ocupado
-        return vehiculo!= null;
+
+    public boolean estaOcupado() { // Indica si el puesto está ocupado
+        return vehiculo != null;
     }
-    
-    public Vehiculo getVehiculo (){ // Retorna el vehiculo ubicado en el puesto
+
+    public Vehiculo getVehiculo() { // Retorna el vehículo ubicado en el puesto
         return vehiculo;
     }
-    public void Ocupar (Vehiculo vehiculo){
+
+    public void ocupar(Vehiculo vehiculo) { // Ubica un vehículo en puesto disponible
         this.vehiculo = vehiculo;
     }
-    public void libre (){ // Deja el puesto libre, para otro vehiculo
+
+    public void libre() { // Deja el puesto libre, para otro vehículo
         this.vehiculo = null;
     }
-    
-    public int getPosicionI(){
+
+    public int getPosicionI() {
         return i;
     }
-    public int getPosicionJ(){
+
+    public int getPosicionJ() {
         return j;
     }
-    
-
-    
 }
