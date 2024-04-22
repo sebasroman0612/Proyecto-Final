9ef820f1cb5d6a0bceeac92eaf570e195102f449
Sebastian Román Sánchez <sebastian.romans@uqvirtual.edu.co>
2024-04-21 20:09:24 -0500

    Update Puesto.java
    Se añadió y modificó la clase Puesto.

--- src/main/java/co/edu/uniquindio/poo/Puesto.java
+++ src/main/java/co/edu/uniquindio/poo/Puesto.java
@@ -5,6 +5,35 @@ package co.edu.uniquindio.poo;
  *
  * @author Sebastian Román - Yefry Fajardo - Santiago Gordillo
  */
-public class Puesto {
+class Puesto {
+    private final int i;
+    private final int j;
+    private Vehiculo vehiculo; // Vehiculo que ocupa un puesto, es null si esta vacío
+    
+    public Puesto (int i, int j){
+        this.i = i;
+        this.j= j;
+        this.vehiculo = null; // Puesto inicia sin vehiculo
+        
+    }
+    public boolean puestoOcupado(){ // Indica si el puesto esta ocupado
+        return vehiculo!= null;
+    }
+    
+    public Vehiculo getVehiculo (){ // Retorna el vehiculo ubicado en el puesto
+        return vehiculo;
+    }
+    public void libre (){ // Deja el puesto libre, para otro vehiculo
+        this.vehiculo = null;
+    }
+    
+    public int getPosicionI(){
+        return i;
+    }
+    public int getPosicionJ(){
+        return j;
+    }
+    
+
     
 }
