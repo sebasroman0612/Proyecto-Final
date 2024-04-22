fbf6da4ac6b656e7ad99c2031e66993117f173ff
Yefry24 <yefrya.fajardog@uqvirtual.edu.co>
2024-04-21 19:45:15 -0500

    Se modifico la clase registro

--- src/main/java/co/edu/uniquindio/poo/Registro.java
+++ src/main/java/co/edu/uniquindio/poo/Registro.java
@@ -10,5 +10,24 @@ package co.edu.uniquindio.poo;
  * @author Sebastian Román - Yefry Fajardo - Santiago Gordillo
  */
 public class Registro {
+    private final Puesto puesto;
+    private final Vehiculo vehiculo;
+    private final LocalDateTime fechaIngreso;
     
+    public Registro(Puesto puesto, Vehiculo vehiculo, LocalDateTime fechaIngreso){
+        this.puesto = puesto;
+        this.vehiculo = vehiculo;
+        this.fechaIngreso = fechaIngreso;
+    } 
+    
+    public Puesto getPuesto(){
+        return puesto;              //Retorna el puesto relacionado con el registro.  
+    }
+    public Vehiculo getVehiculo(){
+        return vehiculo;             //Retorna el vehiculo relacionado con el registro.
+    }
+
+    public LocalDateTime getfechaIngreso(){
+        return fechaIngreso;                    //Retorna la fecha y hora del ingreso.
+    }
 }
